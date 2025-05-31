25ea85e5bf0353eba630d253d5f072cc55979dac
Ankit Yadav <ankit2004.madhuri@gmail.com>
2025-05-31 22:25:04 +0530

    bug

--- client/src/utils/Socket.js
+++ client/src/utils/Socket.js
@@ -1,6 +1,8 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = process.env.REACT_APP_BACKEND_URL; // Change this to production URL if needed
+// const SOCKET_URL = process.env.REACT_APP_BACKEND_URL; // Change this to production URL if needed
+
+const SOCKET_URL = 'http://localhost:5000'; // Default to local development URL
 
 export const socket = io(SOCKET_URL, {
     autoConnect: false, // we'll connect manually after user login or context init
